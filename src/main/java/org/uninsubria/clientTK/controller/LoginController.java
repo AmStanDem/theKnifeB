f6d989752ec85332d6df3ab5cb7f979f91faa8d1
ilGiova19 <giovanni.albe2005@gmail.com>
2026-08-17 10:27:22 +0200

    feat: aggiunto controller Login

---
+++ src/main/java/org/uninsubria/clientTK/controller/LoginController.java
@@ -0,0 +1,41 @@
+package org.uninsubria.clientTK.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginController {
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+
+
+    @FXML
+    private void handleSignIn(ActionEvent event) {
+        try {
+            Stage stage = (Stage) ((Node) event.getSource())
+                    .getScene()
+                    .getWindow();
+
+            stage.setScene(new Scene(
+                    FXMLLoader.load(
+                            getClass().getResource("/org/uninsubria/clientTK/views/MainLayout.fxml")
+                    )
+            ));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
