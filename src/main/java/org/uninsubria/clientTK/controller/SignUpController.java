d130d06f61cec91659ae04db03d10a3ea8e4ea27
ilGiova19 <giovanni.albe2005@gmail.com>
2026-08-17 10:36:06 +0200

    feat: aggiunta schermata login funzionante e collegata con le altre pagine

--- src/main/java/org/uninsubria/clientTK/controller/SignUpController.java
+++ src/main/java/org/uninsubria/clientTK/controller/SignUpController.java
@@ -1,9 +1,16 @@
 package org.uninsubria.clientTK.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javafx.scene.input.MouseEvent;
+import java.io.IOException;
 
 public class SignUpController {
 
@@ -46,5 +53,24 @@ public class SignUpController {
         System.out.println("Cognome: " + cognome);
         System.out.println("Ruolo: " + ruolo);
         System.out.println("Email: " + email);
+    }
+
+
+
+    public void handleAccedi(MouseEvent mouseEvent) {
+        try {
+            Stage stage = (Stage) ((Node) mouseEvent.getSource())
+                    .getScene()
+                    .getWindow();
+
+            stage.setScene(new Scene(
+                    FXMLLoader.load(
+                            getClass().getResource("/org/uninsubria/clientTK/views/LoginView.fxml")
+                    )
+            ));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
