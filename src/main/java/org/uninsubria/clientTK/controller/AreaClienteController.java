321a1bacc7e128a6630eeec1af6fbfcb7a70c4e8
ilGiova19 <giovanni.albe2005@gmail.com>
2026-08-18 11:13:08 +0200

    Feat: aggiunta areaCliente (fxml, css, controller)

---
+++ src/main/java/org/uninsubria/clientTK/controller/AreaClienteController.java
@@ -0,0 +1,73 @@
+package org.uninsubria.clientTK.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AreaClienteController {
+
+    @FXML
+    private TextField txtLocalita;
+
+    @FXML
+    private Button btnGeolocalizzazione;
+
+    @FXML
+    private void onRicercaClick(ActionEvent event) {
+        System.out.println("Ricerca cliccata");
+    }
+
+    @FXML
+    private void onAreaPersonaleClick(ActionEvent event) {
+        try {
+            Stage stage = (Stage) ((Node) event.getSource())
+                    .getScene()
+                    .getWindow();
+
+            stage.setScene(new Scene(
+                    FXMLLoader.load(
+                            getClass().getResource("/org/uninsubria/clientTK/views/SignUp.fxml")
+                    )
+            ));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onGeolocalizzazioneClick() {
+    }
+
+    @FXML
+    public void onRicercaAvanzataClick(ActionEvent actionEvent) {
+        try {
+            Stage stage = (Stage) ((Node) actionEvent.getSource())
+                    .getScene()
+                    .getWindow();
+
+            stage.setScene(new Scene(
+                    FXMLLoader.load(
+                            getClass().getResource("/org/uninsubria/clientTK/views/RicercaAvanzataView.fxml")
+                    )
+            ));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void onPreferitiClick(ActionEvent actionEvent) {
+    }
+
+    public void onRecensioniClick(ActionEvent actionEvent) {
+    }
+}
