c1a9380b4d7acfb07c1767f2e9d32b521b719da5
ilGiova19 <giovanni.albe2005@gmail.com>
2026-08-19 16:50:51 +0200

    Feat: aggiunto pulsante sul logo per tornare alla pagina main in login

--- src/main/java/org/uninsubria/clientTK/controller/LoginController.java
+++ src/main/java/org/uninsubria/clientTK/controller/LoginController.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -31,6 +32,23 @@ public class LoginController {
             stage.setScene(new Scene(
                     FXMLLoader.load(
                             getClass().getResource("/org/uninsubria/clientTK/views/MainLayoutLoggato.fxml")
+                    )
+            ));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handleLogoClick(MouseEvent mouseEvent) {
+        try {
+            Stage stage = (Stage) ((Node) mouseEvent.getSource())
+                    .getScene()
+                    .getWindow();
+
+            stage.setScene(new Scene(
+                    FXMLLoader.load(
+                            getClass().getResource("/org/uninsubria/clientTK/views/MainLayout.fxml")
                     )
             ));
 
